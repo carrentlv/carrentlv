f35b76b1828dd33e65e3558b08e65581f570a7d3
carrentlv <petikepetike122@gmail.com>
2026-05-29 15:21:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+console.log("script fut!");
 
+document.addEventListener("DOMContentLoaded", () => {
+  const registerBtn = document.getElementById("registerBtn");
+  const loginBtn = document.getElementById("loginBtn");
+
+  registerBtn?.addEventListener("click", () => {
+    alert("Regisztráció gomb működik!");
+  });
+
+  loginBtn?.addEventListener("click", () => {
+    alert("Login gomb működik!");
+  });
+});
