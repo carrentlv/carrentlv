bf81f2a3d5d76e93741fed171ce5325538255c87
carrentlv <petikepetike122@gmail.com>
2026-05-29 15:15:16 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "IDE JÖN",
+  authDomain: "IDE JÖN",
+  projectId: "IDE JÖN",
+  storageBucket: "IDE JÖN",
+  messagingSenderId: "IDE JÖN",
+  appId: "IDE JÖN"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
