d9eca1846ef55950bcc74b2f696c5fc7a807be12
carrentlv <petikepetike122@gmail.com>
2026-05-30 14:36:09 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdGjI3Cqw_lw8O8KPMK1CpOiFfVslkAo4",
+  authDomain: "carrentlv-c03b7.firebaseapp.com",
+  projectId: "carrentlv-c03b7",
+  storageBucket: "carrentlv-c03b7.firebasestorage.app",
+  messagingSenderId: "348473215612",
+  appId: "1:348473215612:web:03e9addf7d9987941717f9",
+  measurementId: "G-NFXPXRQEM2"
+};
